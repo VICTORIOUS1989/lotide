04115f317472390fb32f71dc32921e23f2687965
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-06-01 10:45:40 +0000

    reafactor with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,11 @@
-const middle = require('../middle');
+const { assert } = require("chai");
+const middle = require("../middle");
 
-console.log(middle ([1,5,6,2]));
+describe("#middle", () => {
+  it("should return [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("should return [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+});
